56d31e6cb64373bf74d1d9ce00f99635dc5d5b0c
vicvhs <vicvhs@gmail.com>
2019-08-30 14:39:44 -0500

    prueba de correo

--- api.sh
+++ api.sh
@@ -1,9 +1,13 @@
 set -e
 
-cd /var/www/html/cursoAutomat
+cd /var/www/html/vw-api
 
 echo "Pulling from Master"
 
 git pull origin master
 
 echo "Pulled successfully from master"
+
+pm2 restart api
+
+echo "API Restarted"
--- server.js
+++ server.js
@@ -12,8 +12,26 @@ app.use(bodyParser.json())
 app.post('/visibilidad', function (req, res) {
   console.log(req.body.ref)
   console.log(req.body.sender.login)
-  deploy(res)
+  deploy(res, './deploy.sh')
 });
+
+app.post('/api', function (req, res) {
+  console.log(req.body.ref)
+  console.log(req.body.sender.login)
+  deploy(res, './api.sh')
+  const send = require('gmail-send')({
+      user: 'visiblidadweb8@gmail.com',
+      pass: 'test1234',
+      to:   'vicvhs@gmail.com',
+      subject: 'deploy completado',
+  });
+  send({
+    text:    'Envio de Vic api se completo sin errores',
+        }, (error, result, fullResult) => {
+          if (error) console.error(error);
+            console.log(result);
+          })
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
